c3eec2f510627bc41132ede40eaaa433758d2761
Océane Raubitschek <oceane@MBP-de-Oceane.lan>
2024-06-22 18:38:15 +0200

    database migrated to postgres

--- BestCalendar/settings.py
+++ BestCalendar/settings.py
@@ -11,19 +11,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2zc16%4n5jh%^p0cv8gn-17(kdbg8aip_87i#$826f@p0lmimk'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-2zc16%4n5jh%^p0cv8gn-17(kdbg8aip_87i#$826f@p0lmimk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = ['*']
 
@@ -72,10 +77,11 @@ TEMPLATES = [
         },
     },
 ]
+
 LANGUAGE_CODE = 'fr-fr'
 USE_I18N = True
-
 USE_L10N = True
+
 WSGI_APPLICATION = 'BestCalendar.wsgi.application'
 
 
@@ -83,12 +89,8 @@ WSGI_APPLICATION = 'BestCalendar.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -123,7 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-import os
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
@@ -135,3 +136,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
